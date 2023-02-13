1545c3b085a1e1b9649582bbf03c647e4efe88da
HaruHnik <shwehnit14@gmail.com>
2023-02-13 17:52:31 +0630

    upload file function added

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from "express";
+import fs from "fs";
+import bodyParser from "body-parser";
+import formidable from "formidable";
+import { v4 as uuidv4 } from "uuid";
 import dotenv from "dotenv";
 const app = express();
 const port = 3000;
+
 dotenv.config();
+app.use(bodyParser.json());
 
 app.use(express.static("public"));
 const apiUrl = process.env.API_URL;
@@ -13,7 +19,7 @@ const html = `
   <body>
     
     <script type="text/javascript">
-    console.log("${apiUrl}");
+    console.log('${apiUrl}');
        localStorage.setItem("apiUrl","${apiUrl}");
        window.location.href = "/";
     </script>
@@ -25,10 +31,30 @@ app.get("/api", (req, res) => {
   res.send(html);
 });
 
+const mark = [{ name: "mark", email: "markeu@gmail.com", age: 24 }];
+
 app.get("/api/users", (req: Request, res: Response) => {
   res.send({ name: "mark", email: "markeu@gmail.com", age: 24 });
 });
 
-app.listen(3000, () => {
+app.post("/api/uploadFile", (req: Request, res: Response) => {
+  console.log(uuidv4());
+  const form = formidable({ multiples: true });
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      res.writeHead(err.httpCode || 400, { "Content-Type": "text/plain" });
+      return res.end(String(err));
+    }
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ fields, files }, null, 2));
+  });
+
+  /*
+  const writeStream = fs.createWriteStream("./test.jpg");
+  req.pipe(writeStream);
+  res.end(); */
+});
+
+app.listen(port, () => {
   console.log("server is running");
 });
--- public/index.js
+++ public/index.js
@@ -1,16 +1,25 @@
+const hasApiUrl = localStorage.getItem("apiUrl");
 const fetchData = async () => {
-  const hasApiUrl = localStorage.getItem("apiUrl");
   if (hasApiUrl) {
     const response = await fetch(`${hasApiUrl}/users`);
     const data = await response.json();
+    console.log(data);
   } else {
     window.location.href = "/api";
   }
 };
 
-const uploadFile = () => {
+const uploadFile = async () => {
   const inputTag = document.getElementById("inputFile");
-  console.log(inputTag);
+  const formData = new FormData();
+  const files = [...inputTag.files];
+  files.forEach((file) => formData.append("files", file));
+  const response = await fetch(`${hasApiUrl}/uploadFile`, {
+    method: "POST",
+    body: formData,
+  });
+  console.log(response);
 };
+
 uploadFile();
 fetchData();
